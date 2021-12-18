9191885d988b41248423e85e5929f08fed5bda57
muhammadumarsaleh <92623313+muhammadumarsaleh@users.noreply.github.com>
2021-12-19 00:27:56 +0800

    Initial commit

---
+++ belajarphp1.php
@@ -0,0 +1,86 @@
+<?php
+
+// Pertemuan 1 - PHP Dasar
+// Sintaks PHP 
+
+// yang akan kita pelajari
+// komentar, standar output, variabel, operator
+// komentar
+// standar output;
+// echo
+// print
+// print_r
+// var_dump
+
+// var_dump("Muhammad Umar");
+
+// string
+// echo "MUHAMMAD UMAR SALEH";
+
+// integer atau angka
+// echo 12345;
+
+// boolean
+// echo true;
+
+// penulisan sintaks PHP, bisa;
+// 1. PHP didalam HTML (direkomedasikan)
+// 2. HTML didalam PHP
+ 
+
+// Variabel dan Tipe Data
+// Variabel
+
+// $q = 10;
+// echo "nilaiku $q";
+
+$nama = "MUHAMMAD UMAR SALENG";
+echo "Halo nama saya $nama";
+
+// operator
+// aritmatika
+// + - * / %
+// echo 2 * 5;
+// atau
+// $x = 10;
+// $y = 5;
+// echo $x * $y;
+
+//penggabung string / concatenation / concat
+// .
+// $nama_depan = "Muhammad";
+// $nama_belakang = "Umar Saleh";
+// echo $nama_depan . " " . $nama_belakang;
+
+// assignment / penugasan
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 10;
+// echo $x;
+
+// $nama = "Muhammad";
+// $nama .= " ";
+// $nama .= "Umar Saleh";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, != 
+// var_dump(1 < 5);
+// var_dump(10 < 5);
+
+// 1 == "1" = true
+
+// identitas
+// ===, !==
+// 1 == "1" = true
+// 1 === "1" = false
+
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// $x = 10;
+// // var_dump($x < 20 && $x % 2 ==0);
+// var_dump($x < 5 || $x % 2 == 0);
+
+?>
---
+++ belajarphp2.php
@@ -0,0 +1,11 @@
+<!-- contoh PHP dalam HTML< -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+</head>
+<body>
+    <p>Selamat Datang <?php echo "ABDULLAH";?></p>
+</body>
+</html>
